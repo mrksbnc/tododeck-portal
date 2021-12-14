b3cdea320b0eb8c20a032dae89e8950f69525f06
Márkus Bence <markus.bence@konasoft.hu>
2021-12-14 13:07:10 +0100

    changes

--- src/components/controls/VerticalNavBar.vue
+++ src/components/controls/VerticalNavBar.vue
@@ -1,32 +1,48 @@
 <template>
   <div
-    class="bg-slate-200 flex flex-col w-full h-full text-white flex items-center rounded-tr-lg rounded-br-lg"
+    class="bg-slate-200 flex flex-col w-full h-full text-white flex items-center rounded-tr-xl rounded-br-xl"
   >
     <div class="h-1/6 w-full rounded-tr-lg text-center pt-4">
-      <span class="text-lg text-mono text-gray-800 font-bold"> .tododeck </span>
+      <span class="text-mono text-gray-800 font-bold text-lg"> .tododeck </span>
     </div>
-    <div class="h-3/6 w-full flex flex-col items-center">
-      <div class="flex flex-col items-center content-center">
-        <div class="h-full w-full ml-3 my-4 cursor-pointer flex flex-col content-center">
-          <div v-for="menu in navBarMenuElements" :key="menu.id" class="text-gray-500 d-flex my-2">
-            <i :class="[menu.icon, 'mr-2']" />
-            <span class="hover:text-gray-800 my-2 w-full">
-              {{ menu.name }}
-            </span>
-          </div>
+    <div class="h-3/6 w-full rounded-tr-lg text-center pt-4">
+      <div class="w-full text-gray-500 d-flex my-2">
+        <div
+          class="h-full w-full my-4 cursor-pointer flex flex-col items-center justify-center hover:text-gray-800"
+        >
+          <i class="fas fa-home mr-2" />
+          <span class="my-2 w-full"> Overview </span>
+        </div>
+        <div
+          class="h-full w-full my-4 cursor-pointer flex flex-col items-center justify-center hover:text-gray-800"
+        >
+          <i class="fas fa-chart-pie mr-2" />
+          <span class="my-2 w-full"> Statistics </span>
+        </div>
+        <div
+          class="h-full w-full my-4 cursor-pointer flex flex-col items-center justify-center hover:text-gray-800"
+        >
+          <i class="fas fa-folder-open mr-2" />
+          <span class="my-2 w-full"> Collections </span>
+        </div>
+        <div
+          class="h-full w-full my-4 cursor-pointer flex flex-col items-center justify-center hover:text-gray-800"
+        >
+          <i class="far fa-calendar-alt mr-2" />
+          <span class="my-2 w-full"> Calendar </span>
         </div>
       </div>
     </div>
     <div class="h-2/6 w-full h-full flex flex-col items-center rounded-br-lg">
       <div class="h-full w-full flex flex-col items-center content-center">
-        <div
-          class="my-4 cursor-pointer h-full flex flex-col h-full w-full ml-3 items-center justify-end"
-        >
-          <div v-for="menu in systemMenuElements" :key="menu.id" class="text-gray-500 d-flex my-2">
-            <i :class="[menu.icon, 'mr-2']" />
-            <span class="hover:text-gray-800 my-2 w-full">
-              {{ menu.name }}
-            </span>
+        <div class="h-full w-full flex flex-col my-4 cursor-pointer items-center justify-end">
+          <div class="text-gray-500 my-2 hover:text-gray-800">
+            <i class="fa fa-cog mr-2" />
+            <span class="w-full"> Settings </span>
+          </div>
+          <div class="text-gray-500 my-2 hover:text-gray-800">
+            <i class="fas fa-sign-out-alt mr-2" />
+            <span class="w-full"> Logout </span>
           </div>
         </div>
       </div>
@@ -41,7 +57,7 @@
 
   export default defineComponent({
     name: 'VerticalNavBar',
-    setup(props) {
+    setup() {
       const navBarMenuElements = ref(menuCollection);
       const systemMenuElements = ref(systemMenuCollection);
 
